d1993ff2d8f29f1b14a21463697aa17df3a47759
Mark <markpmulder@gmail.com>
2022-03-31 00:32:34 +0200

    background + player rendering correctly

--- flappy.py
+++ flappy.py
@@ -3,72 +3,56 @@ from pyglet.window import key
 from pyglet.window import mouse
 
 window = pyglet.window.Window()
+fps_display = pyglet.window.FPSDisplay(window=window)
 main_batch = pyglet.graphics.Batch()
 bg_batch = pyglet.graphics.Batch()
 game_objects = []
-# player = None
+background = []
+
+winsize = window.get_size()
+player_x_start = (winsize[0]/3)
+player_y_start = (winsize[1]/2)
+
+gravspeed = 10
 
 def init():
-    # Set up window
-    fps_display = pyglet.window.FPSDisplay(window=window)
-    # game_objects = player
-
-    # load assets
-    jumpsnd = pyglet.resource.media('assets/snd/wing.wav')
+    music = pyglet.resource.media('assets/snd/star60.wav')
+    music.play()
     drawBackground()
     drawPlayer()
 
-
-# sound example
-# sound = pyglet.resource.media('assets/audio/hit.wav', streaming=False)
-# sound.play()
-music = pyglet.resource.media('assets/snd/star60.wav')
-music.play()
-
 def drawPlayer():
     global player
-    # player.dx = 1 #00.0
     playerImage = pyglet.resource.image('assets/img/yellowbird-midflap.png')
     player = pyglet.sprite.Sprite(playerImage, batch=main_batch)
-
+    player.x = 0
+    player.y = 0
+    player.update(player_x_start, player_y_start)
 
 def drawBackground():
+    global background
     backgroundImage = pyglet.resource.image('assets/img/background.png')
-    background = pyglet.sprite.Sprite(backgroundImage, batch=bg_batch)
-    winx = window.get_size()[1]
-    bginfo = background.image.get_image_data()
-    windowfit = int((winx/bginfo.width)+2)
-    for i in range(windowfit):
-        newwinwidth = (i*bginfo.width)
-    #     background.blit(newwinwidth,0)
-
-# # debug
-# event_logger = pyglet.window.event.WindowEventLogger()
-# window.push_handlers(event_logger)
+    backgroundMultiplayer = int((window.get_size()[0]/backgroundImage.width)+1)
+    for i in range(backgroundMultiplayer):
+        background.append(pyglet.sprite.Sprite(backgroundImage, (i*backgroundImage.width), backgroundImage.y, batch=bg_batch))
 
 def jump():
+    global jumpsnd, dt
+    jumpsnd = pyglet.resource.media('assets/snd/wing.wav')
     jumpsnd.play()
-    player.sprite.Spriterotation(-10)
+    player.y = player.y + ((gravspeed**2)*dt)
+    # player.sprite.Spriterotation(-10)
 
 
 def update(dt):
-    player.update(dt)
-    # global player
-    winx = window.get_size()[1]
-    print (player.position)
-    playx = player.position[0]
-    while (player.position[0] < winx/3):
-        player.x += player.x * dt
-        print(player.x)
-        print(player.position[0])
-        print(winx/3)
-pyglet.clock.schedule_interval(update, 1/60.0)
-
-# flappy = PhysicalObject(img)
+    # player.x += player.x * dt
+    player.y = player.y - ((gravspeed**2)*dt)
+    # player.y = player.y - (gravspeed*(dt**2))
 
 
 @window.event
 def on_key_press(symbol, modifiers):
+    global player_x, player_y
     if symbol == key.SPACE:
         print('Jump key was pressed')
         jump()
@@ -78,10 +62,14 @@ def on_draw():
     window.clear()
     bg_batch.draw()
     main_batch.draw()
-
-
+    fps_display.draw()
 
 if __name__ == "__main__":
     init()
 
+    pyglet.clock.schedule_interval(update, 1/60.0)
     pyglet.app.run()
+
+# # debug
+# event_logger = pyglet.window.event.WindowEventLogger()
+# window.push_handlers(event_logger)
